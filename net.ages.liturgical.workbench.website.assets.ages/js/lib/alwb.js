a01552f884efdfa72a4791ff78ebb875411fe2f9
Unknown <seraphimdedes@gmail.com>
2019-01-01 12:50:50 -0600

    e

--- net.ages.liturgical.workbench.website.assets.ages/js/lib/alwb.js
+++ net.ages.liturgical.workbench.website.assets.ages/js/lib/alwb.js
@@ -113,48 +113,54 @@ $('.fa-caret-square-o-right.ages-col-picker').show();
 displayingBilingual = false;
 }
 
-function editTextIndex() {
-    $(document).ready(function() {
-        $.ajax({
-            //  url: 'js/lib/indexcorrector.json',
-            url: 'indexcorrector.json',
-            success: function(data) {
-
-                for (var i = 0; i < data.items.length; i++) {
-                    var type = $("a.index-file-link").attr('href');
-
-                    try {
-
-                        if (type.includes(data.items[i].date)) {
-
-                            var x = document.getElementsByClassName("index-service-day");
-                            var j = 0;
-                            while (j < data.items.length) {
-                                var k = 0;
-                                while (k < data.items.length) {
-                                    if (x[j].textContent == data.items[k].prevText && data.items[i].date == data.items[k].date) {
-
-                                        x[j].innerHTML = data.items[k].altText;
-
-                                    }
-                                    k++;
-                                }
-                                j++;
-                            }
-
-                        }
-                    } catch (e) {
-
-                    }
-                }
+function editTextIndex() { 
 
 
-            }
-        });
-    });
-}
+	$(document).ready(function() {
 
-editTextIndex();
+		$.ajax({
+		url: 'indexcorrector.json',
+		success: function(data) {
+		      console.log(data);
+		      
+
+		    $.each(data, function(index, value) {
+		      		
+		      		try {
+	    			var type = $("a.index-file-link").attr('href');
+	    			var newType = type.substring(4,8) + type.substring(9,11) + type.substring(12,14);
+	    			
+
+	    			if (newType.includes(index)) {
+			        
+			      		var x = document.getElementsByClassName("index-service-day");
+				            
+						$.each(value, function(i, v) {	
+				    		for (var i = 0; i < x.length; i++) {         
+				    			if(x[i].textContent == v.prevText) {
+				                
+				        		 x[i].innerHTML = v.altText;
+				    			}
+				    		}           
+						});
+				              
+				            
+			        	
+			    	}
+			    	} catch (e) {
+			        		
+			       	}  
+			});
+			  
+		}
+
+		   
+		
+		});
+	});
+	}
+
+	editTextIndex();
 
 function showAll() {
 	$("div.media-group-empty").css("display","none");
